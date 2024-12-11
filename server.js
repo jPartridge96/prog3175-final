c2a30567327e65727ea9b35b35ef687b93a11c88
Jordan Partridge <jordan.partridge96@hotmail.ca>
2024-12-11 15:27:42 -0500

    Change moviesDb directory path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 8080;
 
-const moviesDb = __dirname + '/data/movies.json';
+const moviesDb = __dirname + '/movies.json';
 var movies;
 
 app.use(express.json());
